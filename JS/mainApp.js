a991074a73d507fc8d4d4b33b41afb1247bac4e5
steingartpeter <steingartpeter@users.noreply.github.com>
2020-12-12 20:20:08 +0100

    2020-12-12 003 X540S Linux
    Some more features

--- JS/mainApp.js
+++ JS/mainApp.js
@@ -2,36 +2,40 @@ var mainApp = mainApp || {};
 
 $(function(){
     console.log("jquery működik");
-    /*$(".gomb").mouseleave(function(){
-        console.log("MOUSE UP");
-        $(".gomb").css({
-            "height":"35px",
-            "width":"45px",
-            "box-shadow":"2px 2px 3px #050505"
-        });
-    })*/
+    $(".gomb").dblclick(function(event){
+        event.preventDefault();
+    });
 });
 
 mainApp.hndlNumericKey = function(me){
     var d = $(me);
     console.log("gomb felirat: "+d.text());
-    $('#szamolas').text($('#szamolas').text()+d.text());
-    $(d).css({
-        "background-color":"#cfcfcf",
-        "box-shadow":"inset 2px 2px 4px #000000",
-        "height":"34px",
-        "width":"44px"
-    });
-    setTimeout(function(){
-        console.log('TIMEOUT!!!');
-        console.log("DE:");
-        console.log(d);
+    var kjlzo = $("#szamolas").text().trim();
+    if(kjlzo.length < 15){
+        console.log("d.text().length: "+d.text().length);
+        $('#szamolas').text(kjlzo+d.text().trim());
         $(d).css({
-            "height":"35px",
-            "width":"45px",
-            "box-shadow":"2px 2px 3px #050505"
+            "background-color":"#cfcfcf",
+            "box-shadow":"inset 2px 2px 4px #000000",
+            "height":"34px",
+            "width":"44px"
         });
-    }, 300, d);
+        setTimeout(function(){
+            console.log('TIMEOUT!!!');
+            console.log("DE:");
+            console.log(d);
+            //$(d).css("");
+            $(d).css({
+                "height":"35px",
+                "width":"45px",
+                "box-shadow":"2px 2px 3px #050505",
+                "background-color":"#dfdfdf"
+            });
+        }, 250, d);
+    }else{
+        console.error("Csak 15 karakternek van hely!");
+    }
+    
 }
 
 mainApp.hndlACBtn = function(me){
