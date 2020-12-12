63863468897e0efc1a84e931e5bb3c32ebb41235
steingartpeter <steingartpeter@users.noreply.github.com>
2020-12-12 19:38:37 +0100

    2020-12-12 002 X540S Linux
    Added some smal features.

--- JS/mainApp.js
+++ JS/mainApp.js
@@ -2,24 +2,44 @@ var mainApp = mainApp || {};
 
 $(function(){
     console.log("jquery működik");
-    $(".gomb").mouseleave(function(){
+    /*$(".gomb").mouseleave(function(){
         console.log("MOUSE UP");
         $(".gomb").css({
             "height":"35px",
             "width":"45px",
             "box-shadow":"2px 2px 3px #050505"
         });
-    })
+    })*/
 });
 
 mainApp.hndlNumericKey = function(me){
     var d = $(me);
     console.log("gomb felirat: "+d.text());
     $('#szamolas').text($('#szamolas').text()+d.text());
-    $(me).css({
+    $(d).css({
         "background-color":"#cfcfcf",
         "box-shadow":"inset 2px 2px 4px #000000",
         "height":"34px",
         "width":"44px"
     });
+    setTimeout(function(){
+        console.log('TIMEOUT!!!');
+        console.log("DE:");
+        console.log(d);
+        $(d).css({
+            "height":"35px",
+            "width":"45px",
+            "box-shadow":"2px 2px 3px #050505"
+        });
+    }, 300, d);
 }
+
+mainApp.hndlACBtn = function(me){
+    
+    var scrnCntn = $("#szamolas").text();
+    //console.log('scrnCntn: '+scrnCntn);
+    if(scrnCntn != ''){
+        $("#szamolas").text(scrnCntn.substr(0,(scrnCntn.length-1)));
+    }
+}
+
