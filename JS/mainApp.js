e915dc56addd64e982568ca786679ce649f637e7
steingartpeter <steingartp.2@gmail.com>
2020-12-12 16:37:07 +0100

    Alap 2020-12-12
    Majdnem fasza számológép

---
+++ JS/mainApp.js
@@ -0,0 +1,25 @@
+var mainApp = mainApp || {};
+
+$(function(){
+    console.log("jquery működik");
+    $(".gomb").mouseleave(function(){
+        console.log("MOUSE UP");
+        $(".gomb").css({
+            "height":"35px",
+            "width":"45px",
+            "box-shadow":"2px 2px 3px #050505"
+        });
+    })
+});
+
+mainApp.hndlNumericKey = function(me){
+    var d = $(me);
+    console.log("gomb felirat: "+d.text());
+    $('#szamolas').text($('#szamolas').text()+d.text());
+    $(me).css({
+        "background-color":"#cfcfcf",
+        "box-shadow":"inset 2px 2px 4px #000000",
+        "height":"34px",
+        "width":"44px"
+    }) ;
+}
